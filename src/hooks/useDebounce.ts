3d50e76aafab75cff933308ce39399650b9e7556
Vinay Bommali <vinay.bommali@netcracker.com>
2026-09-18 16:18:13 +0530

    updated debounce

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,14 +1,14 @@
-import { useCallback, useRef } from "react"
+import { useRef } from "react"
 
 export const useDebounce = (delay: number) => {
     const ref = useRef<ReturnType<typeof setTimeout> | null>(null);
 
-    return useCallback((cb: () => void) => {
+    return (cb: () => void) => {
         if (ref.current) {
             clearTimeout(ref.current);
         }
         ref.current = setTimeout(() => {
             cb();
         }, delay);
-    }, [delay]);
+    };
 }
